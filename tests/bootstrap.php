64079e744e5487bf1a5134e0ab65133248ec5f68
jinjin <jinjin.xu@gsma.com>
2020-05-28 15:39:00 +0100

    remove echo

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,11 +6,9 @@
  */
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
-
 if ( ! $_tests_dir ) {
 	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
 }
-
 if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
 	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	exit( 1 );
@@ -27,6 +25,5 @@ function _manually_load_plugin() {
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
-echo $_tests_dir;
 // Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
