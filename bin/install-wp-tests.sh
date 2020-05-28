e318576593e8009b6ceabe37ff2303c1d59baee2
jinjin <jinjin.xu@gsma.com>
2020-05-28 10:47:20 +0100

    change back to svn

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -107,10 +107,10 @@ install_test_suite() {
 	if [ ! -d $WP_TESTS_DIR ]; then
 		# set up testing suite
 		mkdir -p $WP_TESTS_DIR
-		git clone git://develop.git.wordpress.org/tests/phpunit/includes/ $WP_TESTS_DIR/includes
-		git clone git://develop.git.wordpress.org/tests/phpunit/data/ $WP_TESTS_DIR/data
-		# svn co --quiet https://develop.svn.wordpress.org/${WP_TESTS_TAG}/tests/phpunit/includes/ $WP_TESTS_DIR/includes
-		# svn co --quiet https://develop.svn.wordpress.org/${WP_TESTS_TAG}/tests/phpunit/data/ $WP_TESTS_DIR/data
+		# git clone git://develop.git.wordpress.org/tests/phpunit/includes/ $WP_TESTS_DIR/includes
+		# git clone git://develop.git.wordpress.org/tests/phpunit/data/ $WP_TESTS_DIR/data
+		svn co --quiet https://develop.svn.wordpress.org/${WP_TESTS_TAG}/tests/phpunit/includes/ $WP_TESTS_DIR/includes
+		svn co --quiet https://develop.svn.wordpress.org/${WP_TESTS_TAG}/tests/phpunit/data/ $WP_TESTS_DIR/data
 	fi
 
 	if [ ! -f wp-tests-config.php ]; then
