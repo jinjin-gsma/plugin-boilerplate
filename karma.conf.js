0e05375fd369f09bbef90801faeadb8d9450e9a0
jinjin <jinjin.xu@gsma.com>
2020-06-04 17:30:07 +0100

    convert spaces to tabs

--- karma.conf.js
+++ karma.conf.js
@@ -1,74 +1,74 @@
 module.exports = function (config) {
-  config.set({
-    basePath: '',
-    exclude: ['src/js/index.js'],
-    files: [
-      { pattern: 'src/js/*.js', watched: true, served: true, included: true },
-      { pattern: 'tests/*.js', watched: true, served: true, included: true }
-    ],
+	config.set({
+		basePath: '',
+		exclude: ['src/js/index.js'],
+		files: [
+			{ pattern: 'src/js/*.js', watched: true, served: true, included: true },
+			{ pattern: 'tests/*.js', watched: true, served: true, included: true }
+		],
 
-    autoWatch: false,
-    singleRun: true,
-    failOnEmptyTestSuite: false,
-    logLevel: config.LOG_WARN,
-    frameworks: ['jasmine'],
-    browsers: ['Chrome'],
-    reporters: ['mocha', 'kjhtml', 'progress', 'coverage'],
+		autoWatch: false,
+		singleRun: true,
+		failOnEmptyTestSuite: false,
+		logLevel: config.LOG_WARN,
+		frameworks: ['jasmine'],
+		browsers: ['Chrome'],
+		reporters: ['mocha', 'kjhtml', 'progress', 'coverage'],
 
-    listenAddress: '0.0.0.0',
-    hostname: 'localhost',
-    port: 9876,
-    retryLimit: 0,
-    browserDisconnectTimeout: 5000,
-    browserNoActivityTimeout: 10000,
-    captureTimeout: 60000,
+		listenAddress: '0.0.0.0',
+		hostname: 'localhost',
+		port: 9876,
+		retryLimit: 0,
+		browserDisconnectTimeout: 5000,
+		browserNoActivityTimeout: 10000,
+		captureTimeout: 60000,
 
-    client: {
-      captureConsole: true,
-      clearContext: false,
-      runInParent: false,
-      useIframe: true,
-      jasmine: {
-        random: false
-      }
-    },
+		client: {
+			captureConsole: true,
+			clearContext: false,
+			runInParent: false,
+			useIframe: true,
+			jasmine: {
+				random: false
+			}
+		},
 
-    webpack: {
-      module: {
-        rules: [
-          {
-            test: /\.js$/i,
-            exclude: /(node_modules)/,
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env']
-            }
-          }
-        ]
-      }
-    },
-    preprocessors: {
-      'src/js/*.js': ['webpack', 'coverage'],
-      'tests/*.js': ['webpack']
-    },
-    webpackMiddleware: {
-      noInfo: true,
-      stats: 'errors-only'
-    },
+		webpack: {
+			module: {
+				rules: [
+					{
+						test: /\.js$/i,
+						exclude: /(node_modules)/,
+						loader: 'babel-loader',
+						options: {
+							presets: ['@babel/preset-env']
+						}
+					}
+				]
+			}
+		},
+		preprocessors: {
+			'src/js/*.js': ['webpack', 'coverage'],
+			'tests/*.js': ['webpack']
+		},
+		webpackMiddleware: {
+			noInfo: true,
+			stats: 'errors-only'
+		},
 
-    mochaReporter: {
-      output: 'noFailures'
-    },
+		mochaReporter: {
+			output: 'noFailures'
+		},
 
-    colors: true,
+		colors: true,
 
-    coverageReporter: {
-        includeAllSources: true,
-        dir: 'coverage/',
-        reporters: [
-            { type: 'html', subdir: 'html' },
-            { type: 'text-summary' }
-        ]
-    }
-  });
+		coverageReporter: {
+				includeAllSources: true,
+				dir: 'coverage/',
+				reporters: [
+						{ type: 'html', subdir: 'html' },
+						{ type: 'text-summary' }
+				]
+		}
+	});
 };
--- webpack.config.js
+++ webpack.config.js
@@ -4,36 +4,36 @@ const path = require( 'path' );
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const config = {
-    entry: {
-        main: ['./src/js/index.js', './src/css/index.scss']
-    },
+		entry: {
+				main: ['./src/js/index.js', './src/css/index.scss']
+		},
 
-    output: {
-        path: path.resolve( __dirname, 'builds' )
-    },
+		output: {
+				path: path.resolve( __dirname, 'builds' )
+		},
 
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ['babel-loader', 'eslint-loader'],
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                  MiniCssExtractPlugin.loader,
-                  'css-loader',
-                  'sass-loader',
-                ],
-            }
-        ],
-    },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-    }),
-  ],
+		module: {
+				rules: [
+						{
+								test: /\.js$/,
+								exclude: /node_modules/,
+								use: ['babel-loader', 'eslint-loader'],
+						},
+						{
+								test: /\.s[ac]ss$/i,
+								use: [
+									MiniCssExtractPlugin.loader,
+									'css-loader',
+									'sass-loader',
+								],
+						}
+				],
+		},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: '[name].css',
+		}),
+	],
 }
 
 module.exports = config;
